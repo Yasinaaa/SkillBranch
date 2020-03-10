42179d829cef7bb04fd955cc0c3c705688673fff
Zalyalova Yasina <zalyalova@infomatika.ru>
2020-03-10 16:13:09 +0300

    Write MarkdownCodeView. Part 1

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/markdown/IMarkdownView.kt
@@ -0,0 +1,8 @@
+package ru.skillbranch.skillarticles.ui.custom.markdown
+
+/**
+ * Created by yasina on 10.03.2020.
+ * Copyright (c) 2018 Infomatica. All rights reserved.
+ */
+interface IMarkdownView {
+}
